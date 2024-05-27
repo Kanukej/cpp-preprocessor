486e1d996185422b675a25b3d47e9646a08a88ca
Alina Rodionova <rodibox@yandex.ru>
2024-05-28 03:06:25 +0600

    add definition of Preprocess func

--- main.cpp
+++ main.cpp
@@ -10,17 +10,79 @@
 using namespace std;
 using filesystem::path;
 
+
 path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
-// напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+bool check_file(path& file_path, const string& in_file, const path& dir) {
+    file_path = dir / in_file;
+    if (filesystem::exists(file_path)) {
+        return true;
+    }
+    return false;
+}
+
+bool find_file(path& file_path, const string& in_file, const vector<path>& include_directories) {
+    for (const auto& d : include_directories) {
+        if (check_file(file_path, in_file, d)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool PrintFileNotFoundError(const string& file_name, const string& from, const size_t line_number) {
+    cout << "unknown include file " << file_name << " at file " << from << " at line " << line_number << endl;
+    return false;
+}
+
+bool Preprocess(const path& in_file, ostream& os, const vector<path>& include_directories) {
+    static regex quote_include(R"(\s*#\s*include\s*\"([^"]*)\"\s*)");
+    static regex corner_include(R"(\s*#\s*include\s*<([^>]*)>\s*)");
+    
+    ifstream in(in_file);
+    if (!in) {
+        return false;
+    }
+    
+    string text;
+    size_t line_number = 0;
+    path file_path;
+    while (getline(in, text)) {
+        smatch m;
+        ++line_number;
+        bool need_check = false;
+        if (regex_match(text, m, quote_include)) {
+            need_check = !check_file(file_path, m.str(1), in_file.parent_path());
+        } else if (regex_match(text, m, corner_include)) {
+            need_check = true;
+        } else {
+            os << text << endl;
+            continue;
+        }
+        if (need_check && !find_file(file_path, m.str(1), include_directories)) {
+            return PrintFileNotFoundError(m.str(1), in_file.string(), line_number);
+        }
+        Preprocess(file_path, os, include_directories);
+    }
+    return true;
+}
+
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
+    if (!filesystem::exists(in_file)) {
+        return false;
+    }
+    ofstream out(out_file);
+    if (!out) {
+        return false;
+    }
+    return Preprocess(in_file, out, include_directories);
+}
 
 string GetFileContents(string file) {
     ifstream stream(file);
 
-    // конструируем string по двум итераторам
     return {(istreambuf_iterator<char>(stream)), istreambuf_iterator<char>()};
 }
 
@@ -87,7 +149,6 @@ void Test() {
                 "\n"
                 "int SayHello() {\n"
                 "    cout << \"hello, world!\" << endl;\n"s;
-
     assert(GetFileContents("sources/a.in"s) == test_out.str());
 }
 
